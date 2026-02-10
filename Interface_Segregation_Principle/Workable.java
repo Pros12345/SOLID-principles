5d4d666884af36bb8a73a9a9ac128b9d17b4da31
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-10 23:03:33 +0530

    Add files via upload

--- Interface_Segregation_Principle/Workable.java
+++ Interface_Segregation_Principle/Workable.java
@@ -1,1 +1,25 @@
+package Interface_Segregation_Principle;
 
+interface Workable {
+	void work();
+}
+
+interface Eatable {
+	void eat();
+}
+
+class Human implements Workable, Eatable {
+	public void work() {
+		System.out.println("Human working");
+	}
+
+	public void eat() {
+		System.out.println("Human eating");
+	}
+}
+
+class Robot implements Workable {
+	public void work() {
+		System.out.println("Robot working");
+	}
+}
