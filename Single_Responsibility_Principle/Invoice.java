a598564f6a8b5d2c2a58f3fdd454ca2f0878ac22
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-10 23:07:29 +0530

    Add files via upload

--- Single_Responsibility_Principle/Invoice.java
+++ Single_Responsibility_Principle/Invoice.java
@@ -1,1 +1,19 @@
+package Single_Responsibility_Principle;
 
+class Invoice {
+	public double calculateTotal() {
+		return 1000;
+	}
+}
+
+class InvoicePrinter {
+	public void print(Invoice invoice) {
+		System.out.println("Printing invoice...");
+	}
+}
+
+class InvoiceRepository {
+	public void save(Invoice invoice) {
+		System.out.println("Saving to DB...");
+	}
+}
