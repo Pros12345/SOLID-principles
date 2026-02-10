601e65847017395a0ea11648169ef83781733039
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-10 23:05:04 +0530

    Add files via upload

--- Liskov_Substitution_Principle/Bird.java
+++ Liskov_Substitution_Principle/Bird.java
@@ -1,1 +1,18 @@
+package Liskov_Substitution_Principle;
 
+class Bird {
+}
+
+interface Flyable {
+	void fly();
+}
+
+class Sparrow extends Bird implements Flyable {
+	public void fly() {
+		System.out.println("Sparrow flying");
+	}
+}
+
+class Ostrich extends Bird {
+	// no fly method
+}
