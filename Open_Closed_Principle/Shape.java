1353d1f293a3fd732759d0e155cd68d366c34172
Prosenjit <86827153+Pros12345@users.noreply.github.com>
2026-02-10 23:06:19 +0530

    Add files via upload

--- Open_Closed_Principle/Shape.java
+++ Open_Closed_Principle/Shape.java
@@ -1,1 +1,36 @@
+package Open_Closed_Principle;
 
+interface Shape {
+	double area();
+}
+
+class Circle implements Shape {
+	double radius;
+
+	Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double area() {
+		return Math.PI * radius * radius;
+	}
+}
+
+class Rectangle implements Shape {
+	double length, width;
+
+	Rectangle(double l, double w) {
+		this.length = l;
+		this.width = w;
+	}
+
+	public double area() {
+		return length * width;
+	}
+}
+
+class AreaCalculator {
+	public double calculate(Shape shape) {
+		return shape.area();
+	}
+}
---
+++ Open_Closed_Principle/Triangle.java
@@ -0,0 +1,15 @@
+//adding later
+package Open_Closed_Principle;
+
+class Triangle implements Shape {
+	double base, height;
+
+	Triangle(double b, double h) {
+		base = b;
+		height = h;
+	}
+
+	public double area() {
+		return 0.5 * base * height;
+	}
+}
